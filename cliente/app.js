72c8cefb6ff2399c981741c6e2399d801aa865a0
Yahir Orpeza <yahir.oropeza@gmail.com>
2017-03-24 20:34:52 -0600

    refactorizacion

--- cliente/app.js
+++ cliente/app.js
@@ -1,7 +1,10 @@
 var angular = require('angular');
-url_base = 'http://localhost/AngSlim/servidor/app.php';
+
+	url_base = 'http://localhost/AngSlim/servidor/app.php';
 // url_base = 'http://localhost/tienda/server/app.php';
 
-angular.module('escuela', []);
+	angular.module('escuela', []);
+
 var url = url_base;
+
 require('./controller/estudiante');
